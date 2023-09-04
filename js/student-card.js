1f26c8eaafb21507fa20c62a28fc13752f90cf7c
Rashen <rashen598@gmail.com>
2023-09-04 19:24:18 +0530

    added footers and search bar to view from

--- js/student-card.js
+++ js/student-card.js
@@ -1,5 +1,5 @@
 "use strict";
-  //----------------fetching data from the backend to front end--------------------
+//----------------fetching data from the backend to front end--------------------
 // let btn = document.getElementById("btn");
 
 // btn.addEventListener("click",function (){
@@ -19,8 +19,7 @@
 //                 alt="pro-pic"
 //                 />
 //             </div>
-    
-    
+
 //             <div class="personal-info">
 //                 <span>${element.name}</span>
 //                 <div class="info">
@@ -52,8 +51,7 @@
 //                     </table>
 //                 </div>
 //             </div>
-    
-    
+
 //             <div class="course-details">
 //                 <span>Course Details</span>
 //                 <div class="info">
@@ -73,18 +71,18 @@
 //                     </table>
 //                 </div>
 //             </div>
-    
+
 //             <div class="buttons">
 //                 <div class="update-btn">
 //                     <button>Update</button>
 //                 </div>
-        
+
 //                 <div class="delete-btn">
 //                     <button>Delete</button>
 //                 </div>
 //             </div>
 //         </div>
-  
+
 //           `
 //       });
 
@@ -95,16 +93,93 @@
 
 let studentCard = document.getElementById("main-div");
 
-// const studentDiv = localStorage.getItem('student-div');
-
-
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
-    // Retrieve studentDiv from local storage
-    const studentDiv = localStorage.getItem('student-div');
-    
-    studentCard.innerHTML = studentDiv;
+    fetch("http://localhost:8080/student")
+    .then((response) => response.json())
+    .then((json) => {
+        let studentCard = document.getElementById("main-div");
 
-});
+      let studentDiv = ``;
+
+      json.forEach((element) => {
+        studentDiv += `
+        <div class="container">
+            <div class="pro-image">
+                <img
+                src="/assets/blank-profile-picture-973460_1280.webp"
+                alt="pro-pic"
+                />
+            </div>
+    
+    
+            <div class="personal-info">
+                <span>${element.name}</span>
+                <div class="info">
+                    <table>
+                        <tr>
+                        <td>Date of Birth</td>
+                        <td>${element.dob}</td>
+                        </tr>
+                        <tr>
+                        <td>Sex</td>
+                        <td>${element.sex}</td>
+                        </tr>
+                        <tr>
+                        <td>Email</td>
+                        <td>${element.email}</td>
+                        </tr>
+                        <tr>
+                        <td>Telephone Number</td>
+                        <td>${element.tpNumber}</td>
+                        </tr>
+                        <tr>
+                            <td>Address</td>
+                            <td>${element.address}</td>
+                        </tr>
+                        <tr>
+                        <td>Nic</td>
+                        <td>${element.nic}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+    
+    
+            <div class="course-details">
+                <span>Course Details</span>
+                <div class="info">
+                    <table>
+                        <tr>
+                        <td>School</td>
+                        <td>${element.school}</td>
+                        </tr>
+                        <tr>
+                        <td>Department</td>
+                        <td>${element.department}</td>
+                        </tr>
+                        <tr>
+                        <td>Specialization</td>
+                        <td>${element.course}</td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+    
+            <div class="buttons">
+                <div class="update-btn">
+                    <button>Update</button>
+                </div>
+        
+                <div class="delete-btn">
+                    <button>Delete</button>
+                </div>
+            </div>
+        </div>
+  
+          `;
+      });
+
+        studentCard.innerHTML = studentDiv;
+    });
+  });
+  
